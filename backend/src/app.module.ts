28ca79d0fb5c981f8515e237c2a521cb9d1daa38
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-11-30 12:11:09 -0300

    update nest main modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WeatherModule } from './weather/weather.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGODB_URI') ||
+          'mongodb://localhost/gdash-test',
+      }),
+    }),
+    WeatherModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
