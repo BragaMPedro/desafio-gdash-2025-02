1d0b6a9c38b4f0685bc3c91bc488f416e9482ebd
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-12-03 19:23:10 -0300

    feat initial auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UsersModule } from './users/users.module';
     }),
     WeatherModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -18,15 +18,15 @@ export class UsersService {
     return this.userModel.find(filter).exec();
   }
 
-  async findOne(id: number): Promise<User | null> {
+  async findOne(id: string): Promise<User | null> {
     return this.userModel.findById(id).exec();
   }
 
-  async update(id: number, updateUserDto: Partial<CreateUserDto>): Promise<User | null> {
+  async update(id: string, updateUserDto: Partial<CreateUserDto>): Promise<User | null> {
     return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
   }
 
-  async remove(id: number): Promise<User | null> {
+  async remove(id: string): Promise<User | null> {
     return this.userModel.findByIdAndDelete(id).exec();
   } 
 }
