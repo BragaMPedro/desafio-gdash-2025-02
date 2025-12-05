ebeb9ba15cb735dcfea20f7bd053ea2fea66f1fc
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-12-05 13:08:31 -0300

    fix token expiration time

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from './constants';
     UsersModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '3600' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AuthController],
